0c206008172106aca68d8607abb2a4bc0c379619
KentaroKoizumi <kentaroskoizumi@gmail.com>
2023-10-09 09:17:30 +0000

    微

--- Application/DataObjects/ResponseModels/Root/GreetingResponseModel.cs
+++ Application/DataObjects/ResponseModels/Root/GreetingResponseModel.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.DataObjects.ResponseModels.Root;
 
 public class GreetingResponseModel
 {
+    [Required]
     public string Message { get; set; }
 }
